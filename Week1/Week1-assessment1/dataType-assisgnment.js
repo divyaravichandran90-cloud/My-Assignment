fd6ac45fe9a2f7280ad7437f46faa63a1f12003e
divyaravichandran90-cloud <divyaravichandran90@gmail.com>
2026-04-26 12:55:54 +0530

    Add files via upload
    Week1 assignment1

---
+++ Week1/Week1-assessment1/dataType-assisgnment.js
@@ -0,0 +1,42 @@
+var numebr = 1234
+console.log(numebr)
+console.log(typeof (numebr))
+
+var name = "Divya"
+console.log(name)
+console.log(typeof (name))
+
+var isAutomated = true
+console.log(isAutomated)
+console.log(typeof (isAutomated))
+
+var data
+console.log(data)
+console.log(typeof (data))
+
+var x = null
+console.log(x)
+console.log(typeof(x))
+
+//using "let"
+
+let firstName = "Divya"
+console.log(firstName)
+console.log(typeof(firstName))
+
+let companyName = "Wipro"
+console.log(companyName)
+console.log(typeof(companyName))
+
+let mobileNumber = 789653098
+console.log(mobileNumber)
+console.log(typeof(mobileNumber))
+
+let isAutomation = false
+console.log(isAutomated)
+console.log(typeof(isAutomated))
+
+let hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof(hasPlaywright))
+
